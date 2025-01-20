f94193665b4395bae3b6cdac73a2c4abb11110f8
Bruno St John <brunost.john@icloud.com>
2025-01-20 16:41:12 +0000

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>PLEASE HELP</title>
+</svelte:head>
 <ModeWatcher />
 {@render children()}
